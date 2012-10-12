f893f1883c28d9de3327543180069fb66e42275d
fnobi <shin@fnobi.com>
2012-10-12 14:17:22 +0900

    ちょっとコードフォーマットとテストをいじっただけ。

--- t/ast-test.js
+++ t/ast-test.js
@@ -11,26 +11,26 @@ var stone_sourcecode = [
 	'sum'
 ].join('\n');
 
-buster.testCase("lexer", {
-	"read": function () {
+buster.testCase('lexer', {
+	'read': function () {
 		var lexer = new Lexer(stone_sourcecode);
 		var read = lexer.read();
 		assert(read == 'sum');
 	},
-	"fill queue": function () {
+	'fill queue': function () {
 		var lexer = new Lexer(stone_sourcecode);
 		lexer.fillQueue(5);
 		assert(lexer.queue);
 	},
-	"read line": function () {
+	'read line': function () {
 		var lexer = new Lexer(stone_sourcecode);
 		lexer.readLine();
 		assert(lexer.queue);
 	}
 });
 
-buster.testCase("ast", {
-	"main": function () {
+buster.testCase('ast', {
+	'main': function () {
 		var lexer = new Lexer('3 + 2 * 5 -5');
 		var p = new ExprParser(lexer);
 		var t = p.expression();
