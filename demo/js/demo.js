1323c9950b7d72802304d34e9c1775b9a6da179b
fnobi <shin@fnobi.com>
2012-10-18 22:57:42 +0900

    デモ環境ととのえた。

--- demo/js/demo.js
+++ demo/js/demo.js
@@ -4,6 +4,7 @@ require(['jquery', '../../lib/main'], function ($) {
 		// フォームがsubmitされたら、言語のinterpreterにtextareaの中身を与える
 		// 結果は、 #demo-resultという要素の中身に書き出す
 
+		// lexerを走らせる
 		$('#demo-form-lexer').submit(function (e) {
 			e.preventDefault();
 
@@ -28,7 +29,8 @@ require(['jquery', '../../lib/main'], function ($) {
 		});
 
 
-		$('#demo-form-ast').submit(function (e) {
+		// ExprParserを走らせる
+		$('#demo-form-exprparser').submit(function (e) {
 			e.preventDefault();
 
 			// ソースコードはフォームのsourcecode欄から取得
@@ -40,6 +42,28 @@ require(['jquery', '../../lib/main'], function ($) {
 
 			$('*[name="result"]', this).first().html('');
 			$('*[name="result"]', this).first().html(t + '');
+		});
+
+
+		// BasicParserを走らせる
+		$('#demo-form-basicparser').submit(function (e) {
+			e.preventDefault();
+
+			// ソースコードはフォームのsourcecode欄から取得
+			var sourcecode = $('*[name="sourcecode"]', this).first().val() || '';
+
+			var lexer = new Lexer(sourcecode);
+			var p = new BasicParser(lexer);
+			var t;
+			var prev;
+
+			$('*[name="result"]', this).first().html('');
+
+			while (lexer.hasMore) {
+				t = p.program();
+				prev = $('*[name="result"]', this).first().html();
+				$('*[name="result"]', this).first().html(prev + (t + ''));
+			}
 		});
 	});
 });
