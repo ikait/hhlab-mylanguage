6520eb09bb98f6ff3d942b9219630b01bd16fa25
fnobi <shin@fnobi.com>
2012-10-19 13:33:47 +0900

    tabviewサンプルをかいている。

--- demo/js/TabView.js
+++ demo/js/TabView.js
@@ -1,7 +1,45 @@
+// タブをつかったレイアウトを管理する
 var TabView = function (selector) {
+	this.$links = $('a', $(selector));
 
+	this.initializeTabs();
 };
 
-var Tab = function () {
+// Tabをすべて認識して、1番最初のものを選択する
+TabView.prototype.initializeTabs = function () {
+	var self = this;
+	var tabs = [];
 
+	this.$links.each(function () {
+		var tab = new Tab(this);
+		$(this).click(function (e) {
+			e.preventDefault();
+			self.selectTab(tab);
+		});
+		tabs.push(tab);
+	});
+
+	this.selectTab(tabs[0]);
+	this.tabs = tabs;
+};
+
+// 引数で指定したtabを選択(1つだけ表示)
+TabView.prototype.selectTab = function (tab) {
+	this.tabs.forEach(function (tab) {
+		tab.hide();
+	});
+	tab.show();
+};
+
+var Tab = function (link) {
+	var self = this;
+	this.$content = $($(link).attr('href'));
+};
+
+Tab.prototype.hide = function () {
+	this.$content.hide();
+};
+
+Tab.prototype.show = function () {
+	this.$content.show();
 };
