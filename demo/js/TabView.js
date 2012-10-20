a2bbe792e643bffc2b1a322ec2b3d06672fd1391
fnobi <shin@fnobi.com>
2012-10-20 11:43:51 +0900

    tabview fnobi版、しあげました。

--- demo/js/TabView.js
+++ demo/js/TabView.js
@@ -1,45 +1,59 @@
 // タブをつかったレイアウトを管理する
 var TabView = function (selector) {
-	this.$links = $('a', $(selector));
-
-	this.initializeTabs();
+	this.setTablinks(selector);
 };
 
-// Tabをすべて認識して、1番最初のものを選択する
-TabView.prototype.initializeTabs = function () {
+// selector以下のaタグをすべて認識して、タブを生成
+// また、1番最初のものを選択する
+TabView.prototype.setTablinks = function (selector) {
 	var self = this;
-	var tabs = [];
+	var $links = $('a', $(selector));
 
-	this.$links.each(function () {
-		var tab = new Tab(this);
-		$(this).click(function (e) {
-			e.preventDefault();
-			self.selectTab(tab);
-		});
-		tabs.push(tab);
+	this.tabs = [];
+
+	$links.each(function () {
+		self.addTab(this);
 	});
 
-	this.selectTab(tabs[0]);
-	this.tabs = tabs;
+	this.selectTab(this.tabs[0]);
 };
 
-// 引数で指定したtabを選択(1つだけ表示)
+// 管理するタブを追加
+TabView.prototype.addTab = function (link) {
+	var tab = new Tab(link);
+	tab.tabview = this;
+	this.tabs.push(tab);
+};
+
+// 引数で指定したタブを選択 (1つだけ表示)
 TabView.prototype.selectTab = function (tab) {
-	this.tabs.forEach(function (tab) {
-		tab.hide();
+	this.tabs.forEach(function (t) {
+		t.hide();
 	});
 	tab.show();
 };
 
+// タブの挙動を管理するクラス
 var Tab = function (link) {
 	var self = this;
-	this.$content = $($(link).attr('href'));
+
+	this.$link = $(link);
+	this.$content = $(this.$link.attr('href'));
+
+	$(this.$link).click(function (e) {
+		e.preventDefault();
+		self.tabview.selectTab(self);
+	});
 };
 
+// タブを非表示
 Tab.prototype.hide = function () {
+	this.$link.removeClass('active');
 	this.$content.hide();
 };
 
+// タブを表示
 Tab.prototype.show = function () {
+	this.$link.addClass('active');
 	this.$content.show();
 };
