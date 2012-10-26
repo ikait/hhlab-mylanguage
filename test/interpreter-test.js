83650cccf521b799c2780982ba3ccd2e1e8d0c85
fnobi <shin@fnobi.com>
2012-10-26 13:57:49 +0900

    変数実装やってみよう。

--- test/interpreter-test.js
+++ test/interpreter-test.js
@@ -1,17 +1,33 @@
+var getInterpreterResult = function (sourcecode) {
+	var lexer = new Lexer(sourcecode);
+	var p = new BasicParser(lexer);
+	var env = new BasicEnv();
+
+	var t; var result;
+	while (lexer.peek(0) != Token.EOF) {
+		t = p.program();
+		result = t.eval(env);
+	}
+
+	return result;
+};
+
 buster.testCase('Interpreter', {
 	'calc': function () {
 		var sourcecode = '3 * 5 - 8 * (2 + 1) / 4';
 
-		var lexer = new Lexer(sourcecode);
-		var p = new BasicParser(lexer);
-		var env = new BasicEnv();
-
-		var t; var result;
-		while (lexer.peek(0) != Token.EOF) {
-			t = p.program();
-			result = t.eval(env);
-		}
-
+		var result = getInterpreterResult(sourcecode);
 		assert.equals(result, 9);
+	},
+
+	'var': function () {
+		var sourcecode = [
+			'a = 3',
+			'a + 2'
+		].join('\n');
+
+		var result = getInterpreterResult(sourcecode);
+		assert.equals(result, 5);
 	}
+
 });
